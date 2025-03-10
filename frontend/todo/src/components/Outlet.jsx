c9b6716e4bc78b965d9be106a5b2dca5842838ff
Bharath s <devbharu04@gmail.com>
2025-03-10 22:29:18 +0530

    front

--- frontend/todo/src/components/Outlet.jsx
+++ frontend/todo/src/components/Outlet.jsx
@@ -4,13 +4,13 @@ import { useState } from "react"
 export default function Outlet(){
 
    const [des,setdes] =useState('');
-    return(<div className=' w-[70vh] h-screen border border-shawdow-lg border-gray-600 border-rounded-lg'>
+    return(<div className=' w-[70vh] m-auto h-screen border border-shawdow-lg border-gray-600 border-rounded-lg'>
 
-        <p>
+        <p className='text-4xl sans-serif'>
             Welcome to todo app
         </p>
 
-       <input type='text' className="border-rounder-md "/>
+       <input type='text' className="aligh-center border border-rounder-md "/>
      <input type='checkbox'/>
      
        
